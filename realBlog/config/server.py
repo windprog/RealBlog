8a0c1d7a4661deae75806187181339f195c12d78
windpro <windprog@gmail.com>
2014-03-15 02:56:16 +0800

    * 修改配置文件

--- realBlog/config/server.py
+++ realBlog/config/server.py
@@ -3,16 +3,16 @@
 DEBUG = True
 
 DATABASE_HOST = '127.0.0.1'
-DATABASE_PORT = 27017
-DATABASE_USERNAME = None
-DATABASE_PASSWORD = None
+DATABASE_PORT = 17117
+DATABASE_USERNAME = "root"
+DATABASE_PASSWORD = "42bopwindpro"
 
-STR_NAME = 'Name'
+STR_NAME = 'Windpro'
 STR_ADMINS = 'Administrators'
 
 BLOGS = {
     '127.0.0.1:8000':{
-        STR_NAME: 'realBlog',
-        STR_ADMINS: 'real',
+        STR_NAME: 'wdblog',
+        STR_ADMINS: 'admin',
     }
 }
